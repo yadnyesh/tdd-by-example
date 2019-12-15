0e94818307fdb42874de55b85d5ec20466314652
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@gmail.com>
2019-12-15 19:54:37 +0530

    Lecture 20: Adding Franc

---
+++ src/main/java/guru/springframework/Franc.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+public class Franc {
+	private int amount;
+	
+	public Franc(int amount) {
+		this.amount = amount;
+	}
+	Franc times (int multiplier) {
+		return new Franc(amount * multiplier);
+	}
+	public boolean equals(Object object) {
+		Franc franc = (Franc) object;
+		return amount == franc.amount;
+	}
+}
