0eb00fbe7012e891f6c8568e02616e2db8fbc8e9
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@gmail.com>
2019-12-15 19:51:00 +0530

    Lecture 19: Private Fields

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -2,12 +2,16 @@ package guru.springframework;
 
 public class Dollar {
 	
-	int amount;
+	private int amount;
 	
 	public Dollar(int amount) {
 	 this.amount = amount;
 	}
 	Dollar times (int multiplier) {
 		return new Dollar(amount * multiplier);
+	}
+	public boolean equals(Object object) {
+		Dollar dollar = (Dollar) object;
+		return amount == dollar.amount;
 	}
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -3,6 +3,7 @@ package guru.springframework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 	
@@ -10,13 +11,15 @@ public class MoneyTest {
 	void testMultiplication() {
 		Dollar five = new Dollar(5);
 		Dollar product = five.times(2);
-		assertEquals(10,product.amount);
+		assertEquals(new Dollar(10),product);
 		product = five.times(3);
-		assertEquals(15, product.amount);
+		assertEquals(new Dollar(15), product);
 	}
 	
 	@Test
 	void testEquality() {
 		assertEquals(new Dollar(5), new Dollar(5));
+		assertNotEquals(new Dollar(5), new Dollar(6));
+		
 	}
 }
